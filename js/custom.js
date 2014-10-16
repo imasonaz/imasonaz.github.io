f3e0f4a40f780dfaa4114873dc0a14425ef5f9bb
Ian M <ian@iansprojects.me>
2014-10-16 08:34:53 +0000

    Pretty dropdowns and colors

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,8 @@
+$(window).load(function(){
 $('.row .btn').on('click', function(e) {
     e.preventDefault();
     var $this = $(this);
     var $collapse = $this.closest('.collapse-group').find('.collapse');
     $collapse.collapse('toggle');
+});
 });
